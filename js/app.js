ab0f8cb83bf1560de65afca97aff510576cda1c8
lesieur221_perso <theo.lesieur@etu.unicaen.fr>
2023-09-16 08:28:14 +0200

    CompteARebours opérationnel mais pas fonctionnel

--- js/app.js
+++ js/app.js
@@ -2,11 +2,17 @@ let tempsTravail = 1500;
 let tempsPause = 300;
 let tempsPauseProlongee = 1500;
 
-let chronometre = document.getElementById("chronometre").textContent = tempsTravail
+let chronometre = document.getElementById("chronometre")
 
-function comteARebours() {
-    chronometre.innerText = tempsTravail
-    tempsTravail--
-    console.log(tempsTravail, chronometre)
+function compteARebours() {
+    let minutes = parseInt(tempsTravail / 60,10)
+    let secondes = parseInt(tempsTravail % 60,10)
+
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
+
+    chronometre.innerText = minutes + ":" + secondes
+    tempsTravail = tempsTravail - 1
+    
 }
-setInterval(comteARebours(),1000)
+
