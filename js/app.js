3192ce339a99ab51256e8d5b9e2c43f3b9d75311
lesieur221_perso <theo.lesieur@etu.unicaen.fr>
2023-09-17 17:24:52 +0200

    V1 Terminé

--- js/app.js
+++ js/app.js
@@ -2,19 +2,46 @@ let tempsTravail = 1500;
 let tempsPause = 300;
 let tempsPauseProlongee = 1500;
 let chronometre = document.getElementById("chronometre")
+let phaseTravail = document.getElementById("travail")
+let phasePause = document.getElementById("pause")
+let phase = 1;
+let tempsPhase = 0;
+
+function gererPhase () {
+    if(phase % 2 == 1) {
+        phaseTravail.style.display = "block"
+        phasePause.style.display = "none"
+    } else {
+        phaseTravail.style.display = "none"
+        phasePause.style.display = "block"
+    }
+}
+
+function gererTempsPhase () {
+    if(phase % 2 == 1) {
+        tempsPhase = tempsTravail
+    } else {
+        tempsPhase = tempsPause
+    }
+}
 
 function compteARebours() {
-    let minutes = parseInt(tempsTravail / 60,10)
-    let secondes = parseInt(tempsTravail % 60,10)
+    gererPhase()
+    let minutes = parseInt(tempsPhase / 60,10)
+    let secondes = parseInt(tempsPhase % 60,10)
     minutes = minutes < 10 ? "0" + minutes : minutes
     secondes = secondes < 10 ? "0" + secondes : secondes
     chronometre.innerText = minutes + ":" + secondes
-    tempsTravail = tempsTravail - 1
-    tempsTravail = tempsTravail <= 0 ? 0 : temps - 1
+    tempsPhase = tempsPhase <= 0 ? 0 : tempsPhase - 1
+    if(tempsPhase == 0) {
+        phase ++;
+        gererTempsPhase()
+    }  
 }
 
 let boutonDemmarrer = document.getElementById("demarrer")
 boutonDemmarrer.addEventListener('click', () => {
+    gererTempsPhase()
     boutonRecommencer.style.display="block"
     boutonDemmarrer.style.display = "none"
     setInterval(compteARebours,1000)
