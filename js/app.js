418e828358b864fbdfa1c511a01f282962258f55
lesieur221_perso <theo.lesieur@etu.unicaen.fr>
2023-09-17 16:43:45 +0200

    Boutons fonctionnel / Reste la phase

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
 let tempsTravail = 1500;
 let tempsPause = 300;
 let tempsPauseProlongee = 1500;
-
 let chronometre = document.getElementById("chronometre")
 
 function compteARebours() {
     let minutes = parseInt(tempsTravail / 60,10)
     let secondes = parseInt(tempsTravail % 60,10)
-
     minutes = minutes < 10 ? "0" + minutes : minutes
     secondes = secondes < 10 ? "0" + secondes : secondes
-
     chronometre.innerText = minutes + ":" + secondes
     tempsTravail = tempsTravail - 1
+    tempsTravail = tempsTravail <= 0 ? 0 : temps - 1
 }
 
 let boutonDemmarrer = document.getElementById("demarrer")
 boutonDemmarrer.addEventListener('click', () => {
-    setInterval(compteARebours(),1000)
+    boutonRecommencer.style.display="block"
+    boutonDemmarrer.style.display = "none"
+    setInterval(compteARebours,1000)
 })
 
 let boutonRecommencer = document.getElementById("recommencer")
+boutonRecommencer.style.display="none"
 boutonRecommencer.addEventListener('click', () => {
     location.reload();
 })
